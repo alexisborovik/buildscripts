5cc9731f2af732bddbc4892f277e732300797216
Alexis Borovik <borovikalexey@mail.ru>
2026-06-09 01:20:34 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,47 +5,58 @@ echo "===================================="
 echo "🧹 CLEANING WORKSPACE..."
 echo "===================================="
 rm -rf .repo/local_manifests
-rm -rf device/xiaomi/spes device/xiaomi/sm6225-common
-rm -rf vendor/xiaomi/spes vendor/xiaomi/sm6225-common
-rm -rf kernel/xiaomi/sm6225 hardware/xiaomi hardware/samsung-ext/interfaces
+rm -rf device/xiaomi vendor/xiaomi kernel/xiaomi hardware/xiaomi hardware/samsung-ext
 
 echo "===================================="
 echo "🚀 INIT EVOLUTION-X (bq2)..."
 echo "===================================="
 repo init -u https://github.com/Evolution-X/manifest -b bq2 --git-lfs
+
+echo "===================================="
+echo "📝 CREATING LOCAL MANIFEST..."
+echo "===================================="
+# Создаем официальный манифест со ВСЕМИ нужными деревьями (включая common)
+mkdir -p .repo/local_manifests
+cat <<EOF > .repo/local_manifests/spes.xml
+<?xml version="1.0" encoding="UTF-8"?>
+<manifest>
+  <project path="device/xiaomi/spes" name="Evolution-X-Devices/device_xiaomi_spes" remote="github" revision="bka" />
+  <project path="device/xiaomi/sm6225-common" name="Evolution-X-Devices/device_xiaomi_sm6225-common" remote="github" revision="bka" />
+  <project path="vendor/xiaomi/spes" name="Evolution-X-Devices/vendor_xiaomi_spes" remote="github" revision="bka" />
+  <project path="vendor/xiaomi/sm6225-common" name="Evolution-X-Devices/vendor_xiaomi_sm6225-common" remote="github" revision="bka" />
+  <project path="kernel/xiaomi/sm6225" name="Evolution-X-Devices/kernel_xiaomi_sm6225" remote="github" revision="bka" />
+  <project path="hardware/xiaomi" name="LineageOS/android_hardware_xiaomi" remote="github" revision="lineage-23.2" />
+  <project path="hardware/samsung-ext/interfaces" name="Roynas-Android-Playground/hardware_samsung-extra_interfaces" remote="github" revision="lineage-23.2" />
+</manifest>
+EOF
+
+echo "===================================="
+echo "💥 NUKING CRAVE CACHE BUGS..."
+echo "===================================="
+# Жестко удаляем кэш Crave для наших репозиториев, чтобы repo не сошел с ума!
+rm -rf .repo/project-objects/Evolution-X-Devices
+rm -rf .repo/project-objects/LineageOS/android_hardware_xiaomi.git
+rm -rf .repo/project-objects/Roynas-Android-Playground
+
+echo "===================================="
+echo "🔄 SYNCING EVERYTHING VIA REPO..."
+echo "===================================="
+# Теперь repo скачает всё идеально чисто и официально!
 /opt/crave/resync.sh
 
 echo "===================================="
-echo "📥 CLONING ALL TREES MANUALLY..."
+echo "🩹 FIXING WINDOWS CRLF BUGS..."
 echo "===================================="
-# Качаем специфичные деревья (spes)
-git clone https://github.com/Evolution-X-Devices/device_xiaomi_spes.git -b bka device/xiaomi/spes
-git clone https://github.com/Evolution-X-Devices/vendor_xiaomi_spes.git -b bka vendor/xiaomi/spes
-
-# Качаем ОБЩИЕ деревья (sm6225-common), без них сборка не пойдет!
-git clone https://github.com/Evolution-X-Devices/device_xiaomi_sm6225-common.git -b bka device/xiaomi/sm6225-common
-git clone https://github.com/Evolution-X-Devices/vendor_xiaomi_sm6225-common.git -b bka vendor/xiaomi/sm6225-common
-
-# Качаем ядро и железо
-git clone https://github.com/Evolution-X-Devices/kernel_xiaomi_sm6225.git -b bka kernel/xiaomi/sm6225
-git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-23.2 hardware/xiaomi
-git clone https://github.com/Roynas-Android-Playground/hardware_samsung-extra_interfaces.git -b lineage-23.2 hardware/samsung-ext/interfaces
-
-echo "===================================="
-echo "🩹 FIXING WINDOWS CRLF BUGS IN DEVICE TREE..."
-echo "===================================="
-# Эта магия найдет все файлы .mk, .bp и .sh в папке устройства и удалит из них виндовские символы \r
-find device/xiaomi -type f -name "*.mk" -exec sed -i 's/\r$//' {} +
-find device/xiaomi -type f -name "*.bp" -exec sed -i 's/\r$//' {} +
-find device/xiaomi -type f -name "*.sh" -exec sed -i 's/\r$//' {} +
+# Лечим кривые файлы мейнтейнера
+find device/xiaomi -type f -name "*.mk" -exec sed -i 's/\r$//' {} + || true
+find device/xiaomi -type f -name "*.bp" -exec sed -i 's/\r$//' {} + || true
+find device/xiaomi -type f -name "*.sh" -exec sed -i 's/\r$//' {} + || true
 
 echo "===================================="
 echo "⚙️ STARTING BUILD..."
 echo "===================================="
 export BUILD_USERNAME=Alexis
 export BUILD_HOSTNAME=CraveCloud
-export DISABLE_ROOMSERVICE=true
-export ROOMSERVICE_DISABLED=true
 
 source build/envsetup.sh
 lunch evolution_spes-userdebug
